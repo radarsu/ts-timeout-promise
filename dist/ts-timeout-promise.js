aafeec637281768552bf51d5b3d2e22554a01e17
Radar Su <rkrohmorath@gmail.com>
2017-06-21 08:15:33 +0200

    up

--- dist/ts-timeout-promise.js
+++ dist/ts-timeout-promise.js
@@ -1,5 +1,7 @@
-import * as _ from 'lodash';
-export const timeout = (interval = 0) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _ = require("lodash");
+exports.timeout = (interval = 0) => {
     let timer;
     const promise = _.extend(new Promise((resolve) => {
         timer = setTimeout(() => resolve(timer), interval);
